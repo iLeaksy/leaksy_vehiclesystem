03cf073003463521c3ba3e1de7d3f7b424d69d5e
leaksy <ttv.leaksydesigns@gmail.com>
2023-03-05 02:13:07 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'Leaksy <leaksy.tebex.io>'
-description 'FiveM Vehicle Speed Editor - discord.com/invite/AmcVRwUjjU'
+description 'FiveM Vehicle Speed Editor'
 version '1.0.0'
 
 client_scripts {
