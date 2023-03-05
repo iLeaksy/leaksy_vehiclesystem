0fd3d7c1b6faa41e387ba67b3528650168ba4f01
leaksy <ttv.leaksydesigns@gmail.com>
2023-03-05 02:12:28 +0100

    Update client.lua
    Added the ability to select between classes or vehicles.

--- client.lua
+++ client.lua
@@ -1,60 +1,85 @@
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(10)
-        local ped = PlayerPedId()
-        local veh = GetVehiclePedIsIn(ped, false)
-        local vehclass = GetVehicleClass(veh)
-        if veh ~= nil then
-            if GetVehicleClass(veh) == 0 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Compacts);
-            elseif GetVehicleClass(veh) == 1 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Sedans);
-            elseif GetVehicleClass(veh) == 2 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.SUVs);
-            elseif GetVehicleClass(veh) == 3 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Coupes);
-            elseif GetVehicleClass(veh) == 4 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Muscle);
-            elseif GetVehicleClass(veh) == 5 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.SportsClassics);	
-            elseif GetVehicleClass(veh) == 6 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Sports);			
-            elseif GetVehicleClass(veh) == 7 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Super);
-            elseif GetVehicleClass(veh) == 8 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Motorcycles);
-            elseif GetVehicleClass(veh) == 9 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.OffRoad);
-            elseif GetVehicleClass(veh) == 10 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Industrial);
-            elseif GetVehicleClass(veh) == 11 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Utility);
-            elseif GetVehicleClass(veh) == 12 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Vans);
-            elseif GetVehicleClass(veh) == 13 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Cycles);
-            elseif GetVehicleClass(veh) == 14 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Boats);
-            elseif GetVehicleClass(veh) == 15 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Helicopters);
-            elseif GetVehicleClass(veh) == 16 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Planes);
-            elseif GetVehicleClass(veh) == 17 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Service);
-            elseif GetVehicleClass(veh) == 18 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Emergency);
-            elseif GetVehicleClass(veh) == 19 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Military);
-            elseif GetVehicleClass(veh) == 20 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Commercial);
-            elseif GetVehicleClass(veh) == 21 then
-                ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Trains);
-			end
-		else
-			Wait(10000)
+    while true do
+        Citizen.Wait(2000) -- miliseconds
+    if Config.Handling == 'classes' then
+       -- print('classes')
+        Citizen.CreateThread(function()
+            while true do
+                Citizen.Wait(10)
+                local ped = PlayerPedId()
+                local veh = GetVehiclePedIsIn(ped, false)
+                local vehclass = GetVehicleClass(veh)
+                if veh ~= nil then
+                    if vehclass == 0 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Compacts);
+                    elseif vehclass == 1 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Sedans);
+                    elseif vehclass == 2 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.SUVs);
+                    elseif vehclass == 3 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Coupes);
+                    elseif vehclass == 4 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Muscle);
+                    elseif vehclass == 5 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.SportsClassics);	
+                    elseif vehclass == 6 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Sports);			
+                    elseif vehclass == 7 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Super);
+                    elseif vehclass == 8 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Motorcycles);
+                    elseif vehclass == 9 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.OffRoad);
+                    elseif vehclass == 10 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Industrial);
+                    elseif vehclass == 11 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Utility);
+                    elseif vehclass == 12 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Vans);
+                    elseif vehclass == 13 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Cycles);
+                    elseif vehclass == 14 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Boats);
+                    elseif vehclass == 15 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Helicopters);
+                    elseif vehclass == 16 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Planes);
+                    elseif vehclass == 17 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Service);
+                    elseif vehclass == 18 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Emergency);
+                    elseif vehclass == 19 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Military);
+                    elseif vehclass == 20 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Commercial);
+                    elseif vehclass == 21 then
+                        ModifyVehicleTopSpeed(GetVehiclePedIsIn(ped, false), Config.Trains);
+                    end
+                else
+                    Wait(10000)
+                end
+            end
+        end)
+elseif Config.Handling == 'vehicles' then
+    --print('vehicles')
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(10)
+            local playerPed = GetPlayerPed(-1)
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            local vehicleModel = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))
+                for i = 1, #Config.CustomSpeeds do                              
+                    if vehicleModel == Config.CustomSpeeds[i].model then
+                        ModifyVehicleTopSpeed(vehicle, Config.CustomSpeeds[i].speed );
+                    end
+                end
         end
-	end
+    end)
+end
+end
+
 end)
 
-
-
+Citizen.CreateThread(function()
+    print('Your current Handling is set to - ',Config.Handling)
+end)
--- config.lua
+++ config.lua
@@ -2,8 +2,11 @@
 -- Please note that all values must be float, meaning the must have .00 after them. 
 -- Example: If Config.SUVs is set to 100, it will not work! It Must be 100.00!!!
 -- The Script works for both vanilla vehicles and modded vehicles. :)
+-- Setting Negatives in the float values can make your vehicle go slower, please do not set it too low otherwise the car won't move at all!
 
 Config = {}
+
+Config.Handling = "vehicles" -- "classes" or "vehicles" - choose between Classes or Certain Vehicles
 
 -- Config.VehiclesClass = EXAMPLEBOOST.00
 Config.Compacts = 100.00
@@ -28,3 +31,12 @@ Config.Emergency  = 100.00
 Config.Military  = 100.00
 Config.Commercial  = 100.00
 Config.Trains = 100.00
+
+-- Config file for Vehicles
+-- NOTES
+-- All model names should be in Capital Letters, small letters will not work!
+-- Example: model = "sultanrs" *WILL NOT WORK*, while model = "SULTANRS" *WILL* work!
+Config.CustomSpeeds = {
+    {model = "SULTANRS", speed = 1004.00},
+    {model = "SULTAN", speed = 104.00}
+  }
