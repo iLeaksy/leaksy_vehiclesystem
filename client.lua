4497f6301eb7cb6907021a77be8939bec68bd602
leaksy <48810550+iLeaksy@users.noreply.github.com>
2024-02-27 18:24:32 +0100

    Optimisation
    Fixed couple of issues, so now the script runs at 0.00 - 0.01 almost all the time!

--- client.lua
+++ client.lua
@@ -1,74 +1,57 @@
 Citizen.CreateThread(
     function()
-        while true do
-            Citizen.Wait(2000) -- miliseconds
-            if Config.Handling == "classes" then
-                local classesConfigs = {
-                    [0] = Config.Compacts,
-                    [1] = Config.Sedans,
-                    [2] = Config.SUVs,
-                    [3] = Config.Coupes,
-                    [4] = Config.Muscle,
-                    [5] = Config.SportsClassics,
-                    [6] = Config.Sports,
-                    [7] = Config.Super,
-                    [8] = Config.Motorcycles,
-                    [9] = Config.OffRoad,
-                    [10] = Config.Industrial,
-                    [11] = Config.Utility,
-                    [12] = Config.Vans,
-                    [13] = Config.Cycles,
-                    [14] = Config.Boats,
-                    [15] = Config.Helicopters,
-                    [16] = Config.Planes,
-                    [17] = Config.Service,
-                    [18] = Config.Emergency,
-                    [19] = Config.Military,
-                    [20] = Config.Commercial,
-                    [21] = Config.Trains
-                }
+        local classesConfigs = {
+            [0] = Config.Compacts,
+            [1] = Config.Sedans,
+            [2] = Config.SUVs,
+            [3] = Config.Coupes,
+            [4] = Config.Muscle,
+            [5] = Config.SportsClassics,
+            [6] = Config.Sports,
+            [7] = Config.Super,
+            [8] = Config.Motorcycles,
+            [9] = Config.OffRoad,
+            [10] = Config.Industrial,
+            [11] = Config.Utility,
+            [12] = Config.Vans,
+            [13] = Config.Cycles,
+            [14] = Config.Boats,
+            [15] = Config.Helicopters,
+            [16] = Config.Planes,
+            [17] = Config.Service,
+            [18] = Config.Emergency,
+            [19] = Config.Military,
+            [20] = Config.Commercial,
+            [21] = Config.Trains
+        }
 
-                Citizen.CreateThread(
-                    function()
-                        while true do
-                            Citizen.Wait(1000) -- 1000 ms = 1seconds //wait 1s
-                            local ped = PlayerPedId()
-                            local veh = GetVehiclePedIsIn(ped, false)
-                            if veh then
-                                local vehclass = GetVehicleClass(veh)
-                                local config = classesConfigs[vehclass]
-                                if config then
-                                    ModifyVehicleTopSpeed(veh, config)
-                                end
-                            else
-                                Citizen.Wait(10000) -- 1000 ms = 1sec // wait 10s
-                            end
-                        end
+        local customSpeeds = {}
+        for i = 1, #Config.CustomSpeeds do
+            customSpeeds[GetHashKey(Config.CustomSpeeds[i].model)] = Config.CustomSpeeds[i].speed
+        end
+
+        while true do
+            Citizen.Wait(1000)
+            local ped = PlayerPedId()
+            if IsPedInAnyVehicle(ped, false) then
+                local veh = GetVehiclePedIsIn(ped, false)
+                if Config.Handling == "classes" then
+                    local vehclass = GetVehicleClass(veh)
+                    local config = classesConfigs[vehclass]
+                    if config then
+                        ModifyVehicleTopSpeed(veh, config)
                     end
-                )
-            elseif Config.Handling == "vehicles" then
-                Citizen.CreateThread(
-                    function()
-                        while true do
-                            Citizen.Wait(1000) -- Wait 5 seconds before checking again
-                            local ped = PlayerPedId()
-                            if IsPedInAnyVehicle(ped, false) then -- Check if the player is in a vehicle
-                                local veh = GetVehiclePedIsIn(ped, false)
-                                local vehicleHash = GetEntityModel(veh)
-                                for i = 1, #Config.CustomSpeeds do
-                                    if vehicleHash == GetHashKey(Config.CustomSpeeds[i].model) then
-                                        local vehicletospeedup = GetVehiclePedIsIn(ped, false)
-                                        ModifyVehicleTopSpeed(vehicletospeedup, Config.CustomSpeeds[i].speed)
-                                    end
-                                end
-                            else
-                                Citizen.Wait(5000) -- Wait another 5 seconds before checking again
-                            end
-                        end
+                elseif Config.Handling == "vehicles" then
+                    local vehicleHash = GetEntityModel(veh)
+                    local speed = customSpeeds[vehicleHash]
+                    if speed then
+                        ModifyVehicleTopSpeed(veh, speed)
                     end
-                )
+                else
+                    print("You have not selected a proper config for what to boost! Please check your *config.lua* file")
+                end
             else
-                print("You have not selected a proper config for what to boost! Please check your *config.lua* file")
+                Citizen.Wait(5000)
             end
         end
     end
