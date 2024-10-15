93dd716ad1f3738a00a8b706b7210f49a9f51e28
leaksy <48810550+iLeaksy@users.noreply.github.com>
2024-10-15 08:04:19 +0200

    Optimization

--- client.lua
+++ client.lua
@@ -1,68 +1,73 @@
-Citizen.CreateThread(
-    function()
-        local classesConfigs = {
-            [0] = Config.Compacts,
-            [1] = Config.Sedans,
-            [2] = Config.SUVs,
-            [3] = Config.Coupes,
-            [4] = Config.Muscle,
-            [5] = Config.SportsClassics,
-            [6] = Config.Sports,
-            [7] = Config.Super,
-            [8] = Config.Motorcycles,
-            [9] = Config.OffRoad,
-            [10] = Config.Industrial,
-            [11] = Config.Utility,
-            [12] = Config.Vans,
-            [13] = Config.Cycles,
-            [14] = Config.Boats,
-            [15] = Config.Helicopters,
-            [16] = Config.Planes,
-            [17] = Config.Service,
-            [18] = Config.Emergency,
-            [19] = Config.Military,
-            [20] = Config.Commercial,
-            [21] = Config.Trains
-        }
+Citizen.CreateThread(function()
+    -- Vehicle classes mapped to config settings
+    local classesConfigs = {
+        [0] = Config.Compacts,
+        [1] = Config.Sedans,
+        [2] = Config.SUVs,
+        [3] = Config.Coupes,
+        [4] = Config.Muscle,
+        [5] = Config.SportsClassics,
+        [6] = Config.Sports,
+        [7] = Config.Super,
+        [8] = Config.Motorcycles,
+        [9] = Config.OffRoad,
+        [10] = Config.Industrial,
+        [11] = Config.Utility,
+        [12] = Config.Vans,
+        [13] = Config.Cycles,
+        [14] = Config.Boats,
+        [15] = Config.Helicopters,
+        [16] = Config.Planes,
+        [17] = Config.Service,
+        [18] = Config.Emergency,
+        [19] = Config.Military,
+        [20] = Config.Commercial,
+        [21] = Config.Trains
+    }
 
-        local customSpeeds = {}
-        for i = 1, #Config.CustomSpeeds do
-            customSpeeds[GetHashKey(Config.CustomSpeeds[i].model)] = Config.CustomSpeeds[i].speed
-        end
+    -- Hash map of custom speeds for quick lookup
+    local customSpeeds = {}
+    for _, vehicle in ipairs(Config.CustomSpeeds) do
+        customSpeeds[GetHashKey(vehicle.model)] = vehicle.speed
+    end
 
-        while true do
-            Citizen.Wait(1000)
-            local ped = PlayerPedId()
-            if IsPedInAnyVehicle(ped, false) then
-                local veh = GetVehiclePedIsIn(ped, false)
-                if Config.Handling == "classes" then
-                    local vehclass = GetVehicleClass(veh)
-                    local config = classesConfigs[vehclass]
-                    if config then
-                        ModifyVehicleTopSpeed(veh, config)
-                    end
-                elseif Config.Handling == "vehicles" then
-                    local vehicleHash = GetEntityModel(veh)
-                    local speed = customSpeeds[vehicleHash]
-                    if speed then
-                        ModifyVehicleTopSpeed(veh, speed)
-                    end
-                else
-                    print("You have not selected a proper config for what to boost! Please check your *config.lua* file")
+    while true do
+        Citizen.Wait(1000) -- Check every second
+        local ped = PlayerPedId()
+
+        if IsPedInAnyVehicle(ped, false) then
+            local veh = GetVehiclePedIsIn(ped, false)
+            
+            if Config.Handling == "classes" then
+                -- Modify speed based on vehicle class
+                local vehClass = GetVehicleClass(veh)
+                local config = classesConfigs[vehClass]
+                if config then
+                    ModifyVehicleTopSpeed(veh, config)
                 end
-            else
-                Citizen.Wait(5000)
-            end
-        end
-    end
-)
 
-Citizen.CreateThread(
-    function()
-        if Config.Handling == "classes" or Config.Handling == "vehicles" then
-            print("Your current Handling is set to -", Config.Handling)
+            elseif Config.Handling == "vehicles" then
+                -- Modify speed based on specific vehicle model
+                local vehicleHash = GetEntityModel(veh)
+                local speed = customSpeeds[vehicleHash]
+                if speed then
+                    ModifyVehicleTopSpeed(veh, speed)
+                end
+
+            else
+                print("Invalid Config.Handling setup. Please check your config.lua file.")
+            end
         else
-            print("Invalid Config.Handling setup, should be classes or vehicles")
+            Citizen.Wait(5000) -- Delay when player is not in a vehicle
         end
     end
-)
+end)
+
+-- Startup config validation
+Citizen.CreateThread(function()
+    if Config.Handling == "classes" or Config.Handling == "vehicles" then
+        print("Current Handling mode:", Config.Handling)
+    else
+        print("Invalid Config.Handling. Use 'classes' or 'vehicles' in your config.")
+    end
+end)
